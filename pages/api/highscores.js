59782939652e7eafcab5a044f7bebd0cb2b5d93c
Felix <felix.sellmair@ilosgroup.com>
2022-12-15 19:52:55 +0100

    sort

--- pages/api/highscores.js
+++ pages/api/highscores.js
@@ -1,36 +1,38 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-// const fs = require('fs');
-// const hsPath = '../../muellScores.json';
 const highscores = [];
 
 export default function handler(req, res) {
-  if (req.method === 'POST') {    
-    // const rawdata = fs.readFileSync(hsPath);
-    // const highscores = JSON.parse(rawdata);
-    // highscores.push({ name: req.body.name, score: req.body.score, date:new Date().toISOString() })
-    // fs.writeFileSync(hsPath, JSON.stringify(highscores));
+  if (req.method === 'POST') {
 
-    const name = req.body.name;
-    const score = req.body.score;
-    
-    let userIdx = -1;
-    if(highscores.length>0) {
-      userIdx = highscores.findIndex(sc => {
+    try {
+      const name = req.body.name;
+      const score = parseInt(req.body.score);
+
+      let userIdx = -1;
+      if(highscores.length>0) {
+        userIdx = highscores.findIndex(sc => {
         return sc.name == name;
       });      
     }
-
+    
     if(userIdx > 0) {
       highscores[userIdx].score = score;
     } else {
       highscores.push({ name: name, score: score });
     }
 
+    highscores = highscores.sort(function(a, b) {
+      return parseInt(a.score) - parseInt(b.score);
+    });
+
+    }
+    catch(e) {
+
+    }
+
     
     res.setHeader('Access-Control-Allow-Credentials', true)
     res.setHeader('Access-Control-Allow-Origin', '*')
-    // another common pattern
-    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
     res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
     res.setHeader(
       'Access-Control-Allow-Headers',
@@ -38,13 +40,8 @@ export default function handler(req, res) {
     )
     res.status(200).json(highscores);
   } else {
-    // Handle any other HTTP method
-    // const rawdata = fs.readFileSync(hsPath);
-    // const highscores = JSON.parse(rawdata);
     res.setHeader('Access-Control-Allow-Credentials', true)
     res.setHeader('Access-Control-Allow-Origin', '*')
-    // another common pattern
-    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
     res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
     res.setHeader(
       'Access-Control-Allow-Headers',

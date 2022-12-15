ce544594570e32e52da3ca8f776d41c1ead9116c
Felix <felix.sellmair@ilosgroup.com>
2022-12-15 17:37:35 +0100

    headers

--- pages/api/highscores.js
+++ pages/api/highscores.js
@@ -10,11 +10,29 @@ export default function handler(req, res) {
     // highscores.push({ name: req.body.name, score: req.body.score, date:new Date().toISOString() })
     // fs.writeFileSync(hsPath, JSON.stringify(highscores));
     highscores.push({ name: req.body.name, score: req.body.score, date:new Date().toISOString() })
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    // another common pattern
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
     res.status(200).json('{ "ok": "cool" }');
   } else {
     // Handle any other HTTP method
     // const rawdata = fs.readFileSync(hsPath);
     // const highscores = JSON.parse(rawdata);
+    res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
     res.status(200).json(highscores);
   }
 }

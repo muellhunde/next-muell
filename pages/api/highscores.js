c5eae5d6b0c0cf695fde75cb51ef9b81cd7809e8
Felix <felix.sellmair@ilosgroup.com>
2022-12-15 19:06:35 +0100

    logic

--- pages/api/highscores.js
+++ pages/api/highscores.js
@@ -9,7 +9,23 @@ export default function handler(req, res) {
     // const highscores = JSON.parse(rawdata);
     // highscores.push({ name: req.body.name, score: req.body.score, date:new Date().toISOString() })
     // fs.writeFileSync(hsPath, JSON.stringify(highscores));
-    highscores.push({ name: req.body.name, score: req.body.score, date:new Date().toISOString() })
+
+    const name = req.body.name;
+    const score = req.body.score;
+
+    let userScore = highscores.filter(sc => {
+      return sc.name == name;
+    })
+
+    if(userScore) {
+      userScore.score = score;
+    } else {
+      highscores.push({ name, score, date: new Date().toDateString() });
+    }
+
+
+
+    
     res.setHeader('Access-Control-Allow-Credentials', true)
     res.setHeader('Access-Control-Allow-Origin', '*')
     // another common pattern
@@ -19,20 +35,20 @@ export default function handler(req, res) {
       'Access-Control-Allow-Headers',
       'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
     )
-    res.status(200).json('{ "ok": "cool" }');
+    res.status(200).json(highscores);
   } else {
     // Handle any other HTTP method
     // const rawdata = fs.readFileSync(hsPath);
     // const highscores = JSON.parse(rawdata);
     res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    // another common pattern
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
     res.status(200).json(highscores);
   }
 }
